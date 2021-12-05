d7a426bddebab3f89bd7e1e220a6b494f0301ecf
mandrejczuk <mandrejczuk99@gmail.com>
2021-12-05 18:00:24 +0100

    dd

---
+++ src/components/Room/Rooms.js
@@ -0,0 +1,149 @@
+import  React, {Component } from "react";
+//import "./Film.css";
+import "bootstrap/dist/css/bootstrap.css";
+import Room from "./Room";
+import { Table, Button } from 'react-bootstrap';
+import { confirmAlert } from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css';
+import nextId from "react-id-generator";
+import RoomClass from "../../class/RoomClass";
+import EditForm from "../../forms/EditForm"
+import DeleteForm from "../../forms/DeleteForm"
+import AddRoom from  "../../AddRoom"
+import {NotificationContainer, NotificationManager} from 'react-notifications';
+import 'react-notifications/lib/notifications.css';
+
+
+class Rooms extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      roomList: [
+       new RoomClass(parseInt(nextId().slice(2)),  30,  24 ),
+       new RoomClass( parseInt(nextId().slice(2)),  32,  9 ),
+        new RoomClass( parseInt(nextId().slice(2)),  33,  13 ),
+        new RoomClass( parseInt(nextId().slice(2)),  34,  9 ),
+      ],
+    };
+  }
+
+
+  createNotification(message) {
+    NotificationManager.success('Success', message);
+  }
+
+  showDeleteForm = (id) => {
+    const { roomList } = this.state;
+    var index = roomList.findIndex(function (value) {
+      return value.id === id;
+    });
+
+    confirmAlert({
+      customUI: ({ onClose }) => {
+        return (
+          <DeleteForm index={index} onClose={onClose} deleteFilm={this.deleteFilm} />
+        );
+      }
+    });
+  }
+
+  showEditForm = (id) => {
+    const { roomList } = this.state;
+    var index = roomList.findIndex(function (value) {
+      return value.id === id;
+    });
+
+    confirmAlert({
+      customUI: ({ onClose }) => {
+        return (
+          <div>
+            <EditForm roomList={roomList} index={index} onClose={onClose} editRoom={this.editRoom} />
+            <NotificationContainer />
+          </div>
+        );
+      }
+    });
+  }
+
+  onChange(e) {
+    var name = e.target.id;
+    this.setState({
+      [name]: e.target.value
+    });
+  }
+
+  changeTable() {
+    this.setState(state => {
+      let show = state.showRooms
+      return { showRooms: !show }
+    });
+  }
+  
+  addRoom = (s) => {
+    this.setState(state => {
+      var rooms = state.roomList;
+      var id = state.roomList.length + 1;
+      let newRoom = new RoomClass(id, s.space, s.spaceLeft);
+      rooms.push(newRoom);
+      return { RoomList: Rooms }
+    });
+  }
+
+  editRoom = (index, s) => {
+    this.setState(state => {
+      var rooms = state.roomList;
+      rooms[index].space = s.editSpace;
+      rooms[index].spaceLeft = s.editSpaceLeft;
+
+      return { roomList: rooms }
+    });
+    this.createNotification('Room was edited successfully');
+  }
+
+  deleteRoom = (index) => {
+    this.setState(state => {
+      var rooms = state.roomList;
+      rooms.splice(index, 1);
+      return { roomList: rooms }
+    });
+  }
+
+
+
+
+  render() {
+    
+   
+    return (
+      <div>
+        <h3>List of rooms</h3>
+        <Table striped bordered>
+          <thead>
+            <tr>
+              <th>id</th>
+              <th>Space</th>
+              <th>SpaceLeft</th>
+              <th>Actions</th>
+            </tr>
+
+            {this.state.roomList.map((room, key) => {
+              return (
+                <Room
+                  key={key}
+                  id={room.id}
+                  space={room.space}
+                  spaceLeft={room.spaceLeft}
+                  showEditForm={this.showEditForm}
+                  showDeleteForm={this.showDeleteForm}
+                />
+              );
+            })}
+          </thead>
+          <tbody></tbody>
+        </Table>
+        <AddRoom addRoom = {this.addRoom }/>
+      </div>
+    );
+  }
+}
+export default Rooms;
---
+++ src/forms/DeleteForm.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import * as Icon from "react-bootstrap-icons";
+
+const DeleteForm = (props) => {
+    const { deleteRoom, onClose, index } = props;
+    return (
+        <div className="alertForm">
+            <span className="closeButton">
+                <Icon.XCircleFill color="dimgray" size={18} onClick={() => onClose()} />
+            </span>
+            <div className="importantInfoInAlert">
+                <Icon.Info size={60} color="#017BFF" />
+                Are you sure you want to delete this room?
+            </div>
+            <div className="roomDeleteButtons">
+            <Button variant="success" style={{ marginLeft: "10px" }}
+                    onClick={() => {
+                        deleteRoom(index);
+                        onClose();
+                    }}>Yes</Button>
+                <Button variant="danger" onClick={() => onClose()}>No</Button>
+            </div>
+        </div>
+    );
+};
+
+export default DeleteForm;
